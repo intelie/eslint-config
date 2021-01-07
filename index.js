db82f2db984af3f59aae366aef1dcf1f72ef5c2c
Rafael Mello Campanari <lab.rafamello@gmail.com>
2021-01-07 18:17:12 -0200

    setting: react version to be detected

--- index.js
+++ index.js
@@ -62,6 +62,11 @@ module.exports = {
             }
         }
     ],
+    "settings": {
+        "react": {
+          "version": "detect"
+        }
+    },
     "globals": {
         "jQuery": true,
         "$": true,
